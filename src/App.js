a8aa549bf6a432241ffbf862c5dba7460b584522
jenna5376 <jenna5376@gmail.com>
2023-10-14 23:31:20 -0400

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import Navbar from "./Navbar";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Footer from "./Footer";
+import Navbar from "./Navbar"
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Footer from "./Footer"
 import { Route, Routes } from "react-router-dom"
-import Bondit from "./pages/CaseStudies/Bondit";
-import Checkmate from "./pages/CaseStudies/Checkmate";
-import Cibi from "./pages/CaseStudies/Cibi";
-import DailyUi from "./pages/CaseStudies/DailyUi";
-import DrawTogether from "./pages/CaseStudies/DrawTogether";
-import EquitableSyllabusProject from "./pages/CaseStudies/EquitableSyllabusProject";
+import Bondit from "./pages/CaseStudies/Bondit"
+import Checkmate from "./pages/CaseStudies/Checkmate"
+import Cibi from "./pages/CaseStudies/Cibi"
+import DailyUi from "./pages/CaseStudies/DailyUi"
+import DrawTogether from "./pages/CaseStudies/DrawTogether"
+import EquitableSyllabusProject from "./pages/CaseStudies/EquitableSyllabusProject"
 import AnimatedCursor from "react-animated-cursor"
+import ReactGA from "react-ga4"
+
+ReactGA.initialize("G-PHX4QMCHRS");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 function App() {
   return (
@@ -30,7 +38,7 @@ function App() {
         color='249, 56, 137'
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
